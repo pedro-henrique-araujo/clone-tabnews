5f590b82d9ca6943fead76d0f5f6aefa7cc85c3f
Pedro <60669964+pedro-henrique-araujo@users.noreply.github.com>
2023-08-12 18:18:56 +0000

    add scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 A project for following up a dev course
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,37 @@
 function Home() {
-    return (<div>
-        <p>
+  return (
+    <div>
+      <p>
+        Minha irmã, riso constante a brilhar,
+        <br />
+        Com teu jeito único de encantar,
+        <br />
+        Cada dia contigo é um festival,
+        <br />
+        Alegria e amor em um astral.
+      </p>
 
-            Minha irmã, riso constante a brilhar,<br />
-            Com teu jeito único de encantar,<br />
-            Cada dia contigo é um festival,<br />
-            Alegria e amor em um astral.
-        </p>
+      <p>
+        Tuas risadas são como música no ar,
+        <br />
+        Contagiam todos, não dá pra evitar,
+        <br />
+        Até as estrelas param para escutar,
+        <br />
+        Minha irmã, és alegria a se multiplicar.
+      </p>
 
-        <p>
-            Tuas risadas são como música no ar,<br />
-            Contagiam todos, não dá pra evitar,<br />
-            Até as estrelas param para escutar,<br />
-            Minha irmã, és alegria a se multiplicar.
-
-        </p>
-
-
-        <p>
-            Então que a vida siga nesse ritmo sem fim,<br />
-            Com tuas risadas a iluminar o caminho,<br />
-            Um presente tão bom quanto um docinho,<br />
-            Minha irmã, és alegria que não tem fim!
-        </p>
-
-    </div>);
+      <p>
+        Então que a vida siga nesse ritmo sem fim,
+        <br />
+        Com tuas risadas a iluminar o caminho,
+        <br />
+        Um presente tão bom quanto um docinho,
+        <br />
+        Minha irmã, és alegria que não tem fim!
+      </p>
+    </div>
+  );
 }
 
 export default Home;
